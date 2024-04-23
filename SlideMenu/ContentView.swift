28fddc2c99631835841bdb82715bcc97b18f7d93
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-04-23 11:24:26 +0330

    Initial Commit

---
+++ SlideMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlideMenu
+//
+//  Created by Omid Heydarzadeh on 4/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlideMenu/SlideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideMenuApp.swift
+//  SlideMenu
+//
+//  Created by Omid Heydarzadeh on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SlideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
